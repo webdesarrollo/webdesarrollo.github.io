74ad641625f0f22a55c926546a313ccab766c74b
WebDesarrollo <37180933+webdesarrollo@users.noreply.github.com>
2020-12-20 15:58:03 -0300

    Add files via upload

---
+++ _next/static/S3Eu0z2Paxhz-ZLjPUE6a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F41d7580151404105cf05.css","static\u002Fchunks\u002Fpages\u002Findex-5cfee4dfb731d3beff3e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-58bbc56ba6d789f4eba3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
